10e5f2460a16f66780bf8527f62d2f3c8242d810
Sharjeel Iqbal <sharjeelsiqbal@gmail.com>
2021-06-15 12:37:19 -0700

    Completed Javascript Objects

---
+++ prep-javascript-objects/main.js
@@ -0,0 +1,13 @@
+var person = {
+  firstName: 'Sharjeel',
+  lastName: 'Iqbal',
+  hobby: 'Basketball'
+};
+
+var fullName = person.firstName + ' ' + person.lastName;
+console.log(fullName);
+person.job = 'Baseball';
+console.log(person.job);
+person.previousJob = 'Cashier';
+console.log(person.previousJob);
+console.log(person);
