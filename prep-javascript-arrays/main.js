a157322e672f8fc6857181980edfb4a59744c524
Sharjeel Iqbal <sharjeelsiqbal@gmail.com>
2021-06-15 12:54:39 -0700

    Completed Javascript Arrays

---
+++ prep-javascript-arrays/main.js
@@ -0,0 +1,7 @@
+var languages = ['html', 'css', 'javascript', 'react', 'node', 'postgres'];
+console.log(languages);
+console.log('The first element of the array is ' + languages[0]);
+console.log('The third element of the array is ' + languages[2]);
+console.log('the length of the array is ' + languages.length);
+var lastElement = languages[languages.length - 1];
+console.log(lastElement);
